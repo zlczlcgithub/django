25844b742a3fffb4b1c60e6c6472ff845e889ad4
jeremy <530234905@qq.com>
2017-11-06 01:02:21 +0900

    tutorial finished

--- joyz-proj/django/mysite/polls/admin.py
+++ joyz-proj/django/mysite/polls/admin.py
@@ -1,6 +1,18 @@
 from django.contrib import admin
-from .models import Question
+from .models import Question,Choice
 
+class ChoiceInline(admin.StackedInline):
+    model = Choice
+    extra = 3
+class QuestionAdmin(admin.ModelAdmin):
+    list_display = ('question_text', 'pub_date', 'was_published_recently')
+    fieldsets = [
+        (None,               {'fields': ['question_text']}),
+        ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
+    ]
+    inlines = [ChoiceInline]
+
+admin.site.register(Question, QuestionAdmin)
 # Register your models here.
-admin.site.register(Question)
+#admin.site.register(Question)
 #admin.site.register(Choice)
--- joyz-proj/django/mysite/polls/views.py
+++ joyz-proj/django/mysite/polls/views.py
@@ -3,27 +3,41 @@ from django.template import loader
 from .models import Question
 from django.http import Http404,HttpResponse,HttpResponseRedirect
 from django.urls import reverse
+from django.views import generic
+from django.utils import timezone
+# generic
+class IndexView(generic.ListView):
+	template_name = 'polls/index.html'
+	context_object_name = 'latest_question_list'
+
+	def get_queryset(self):
+		return Question.objects.\
+		filter(pub_date__lte=timezone.now()).order_by('-pub_date')[:5]
+
+class DetailView(generic.DetailView):
+	model = Question
+	template_name = 'polls/detail.html'
+
+class ResultsView(generic.DetailView):
+	model = Question
+	template_name = 'polls/results.html'
+
 
 # Create your views here.
+'''
 def index(request):
     latest_question_list\
 	 = Question.objects\
 		   .order_by('-pub_date')[:5]
-    #template = loader.get_template('polls/index.html')
     context = {'latest_question_list':latest_question_list,}
-    #output = ', '.join([q.question_text for q in latest_question_list])
-    #return HttpResponse(template.render(context,request))
     return render(request, 'polls/index.html',context)
-
+'''
 def detail(request, question_id):
     question = get_object_or_404(Question, pk = question_id)
     #msg = "You are looking at question {}." 
     #return HttpResponse(msg.format(question_id))	
     return render(request,'polls/detail.html',{'question':question}) 
 
-def results(request, question_id):
-    response = "You are looking at the results of question {}."
-    return HttpResponse(response.format(question_id))
 
 def vote(request, question_id):
 	question = get_object_or_404(Question, pk = question_id)
